4fc8946e8266dda1dde4d9d6eddfb13e2e11b992
Adam O'Neil <adamosoftware@gmail.com>
2020-10-04 16:55:47 -0400

    removed obsolete line

--- Tests/Chapter08/StarWarsTestsLocal.cs
+++ Tests/Chapter08/StarWarsTestsLocal.cs
@@ -11,12 +11,6 @@ namespace Tests.Chapter08
     public class StarWarsTestsLocal
     {
         /// <summary>
-        /// HttpClient should always be static due to subtleties about how it uses resources internally
-        /// see https://docs.microsoft.com/en-us/aspnet/web-api/overview/advanced/calling-a-web-api-from-a-net-client#create-and-initialize-httpclient
-        /// </summary>
-        static HttpClient _client = new HttpClient();
-
-        /// <summary>
         /// before running this, launch an instance of Chapter08.Service (set project as startup, press Ctrl+F5 to start without debugger)
         /// </summary>
         [TestMethod]
