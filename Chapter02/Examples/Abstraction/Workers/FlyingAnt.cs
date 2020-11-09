da41e57d41aeefb022340ef09614c75f1a5bf925
Almantas Karpavičius <almantusk@gmail.com>
2020-11-09 08:09:44 +0200

    Cleaned up unused namespaces

--- Chapter02/Examples/Abstraction/Workers/FlyingAnt.cs
+++ Chapter02/Examples/Abstraction/Workers/FlyingAnt.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Chapter02.Examples.Abstraction.Workers
 {
--- Chapter02/Examples/Abstraction/Workers/IFlyer.cs
+++ Chapter02/Examples/Abstraction/Workers/IFlyer.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Chapter02.Examples.Abstraction.Workers
+﻿namespace Chapter02.Examples.Abstraction.Workers
 {
     public interface IFlyer
     {
