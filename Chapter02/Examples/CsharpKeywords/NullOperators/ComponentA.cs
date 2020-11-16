3b45132382b6c5e3e6e3caef9675d2d370d9f4d5
Almantas Karpavičius <almantusk@gmail.com>
2020-11-16 06:57:21 +0200

    Added null operators examples

---
+++ Chapter02/Examples/CsharpKeywords/NullOperators/ComponentA.cs
@@ -0,0 +1,6 @@
+﻿namespace Chapter02.Examples.CsharpKeywords.NullOperators
+{
+    class ComponentA
+    {
+    }
+}
---
+++ Chapter02/Examples/CsharpKeywords/NullOperators/Demo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chapter02.Examples.Encapsulation;
+
+namespace Chapter02.Examples.CsharpKeywords.NullOperators
+{
+    public static class Demo
+    {
+        public static void Run()
+        {
+            var componentB = new ComponentB(new ComponentA());
+
+            var dog1 = new Dog("Sparky");
+            if (dog1.Owner != null)
+            {
+                bool ownerNameStartsWithA = dog1.Owner.StartsWith('A');
+            }
+            dog1.Owner?.StartsWith('A');
+
+            string description;
+            if (dog1.Owner == null)
+            {
+                description = dog1.Name;
+            }
+            else
+            {
+                description = $"{dog1.Name}, dog of {dog1.Owner}";
+            }
+
+            description = dog1.Owner == null
+                ? dog1.Name
+                : $"{dog1.Name}, dog of {dog1.Owner}";
+
+            var dog2 = new Dog("Speedy");
+
+            if (dog1.Owner != null)
+            {
+                dog2.Owner = dog1.Owner;
+            }
+
+            dog1.Owner ??= dog2.Owner;
+        }
+    }
+}
