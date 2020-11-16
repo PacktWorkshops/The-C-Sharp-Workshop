d993bff27bf74056625255318696e73606e6a5da
Almantas Karpavičius <almantusk@gmail.com>
2020-11-16 06:57:36 +0200

    Added operators overload example

---
+++ Chapter02/Examples/CsharpKeywords/Operators/BankAccount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter02.Examples.CsharpKeywords.Operators
+{
+    public class BankAccount
+    {
+        private decimal _balance;
+
+        public BankAccount(decimal balance)
+        {
+            _balance = balance;
+        }
+
+        public static bool operator >(BankAccount account1, BankAccount account2)
+            => account1?._balance > account2?._balance;
+
+        public static bool operator <(BankAccount account1, BankAccount account2)
+            => account1?._balance < account2?._balance;
+
+        public static bool operator ==(BankAccount account1, BankAccount account2)
+            => account1?._balance == account2?._balance;
+
+        public static bool operator !=(BankAccount account1, BankAccount account2)
+            => !(account1 == account2);
+
+        public static bool operator *(BankAccount account1, BankAccount account2)
+        {
+            return false;
+        }
+    }
+}
---
+++ Chapter02/Examples/CsharpKeywords/Operators/OperatorsDemo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter02.Examples.CsharpKeywords.Operators
+{
+    public class OperatorsDemo
+    {
+        public static void Run()
+        {
+            var account1 = new BankAccount(-1.01m);
+            var account2 = new BankAccount(1.01m);
+            var account3 = new BankAccount(1001.99m);
+            var account4 = new BankAccount(1001.99m);
+
+            Console.WriteLine(account1 == account2);
+            Console.WriteLine(account1 != account2);
+            Console.WriteLine(account2 > account1);
+            Console.WriteLine(account1 < account2);
+            Console.WriteLine(account3 == account4);
+            Console.WriteLine(account3 != account4);
+        }
+    }
+}
