56a618438350ebfbaf52e3737ac1ef3efa19e9a1
Almantas Karpavičius <almantusk@gmail.com>
2020-11-17 06:19:33 +0200

    Custom list tests

--- Chapter02/Examples/CsharpKeywords/Generics/CustomList.cs
+++ Chapter02/Examples/CsharpKeywords/Generics/CustomList.cs
@@ -6,8 +6,50 @@ using System.Threading.Tasks;
 
 namespace Chapter02.Examples.CsharpKeywords.Generics
 {
-    public class CustomList
+    public class CustomList<T>
     {
+        public T[] Items { get; set; }
 
+        public int Count => Items.Length;
+
+        public CustomList()
+        {
+            Items = new T[0];
+        }
+
+        public CustomList(T[] items)
+        {
+            Items = new T[items.Length];
+            Array.Copy(items, Items, items.Length);
+        }
+
+        public void Add(T item)
+        {
+            var expanded = new T[Items.Length+1];
+            Array.Copy(Items, expanded, Items.Length);
+            expanded[^1] = item;
+            Items = expanded;
+        }
+
+        public void RemoveAtIndex(int index)
+        {
+            if (index >= Items.Length)
+            {
+                throw new IndexOutOfRangeException($"{index} is more than last index {Items.Length-1}");
+            }
+
+            var shrunk = new T[Items.Length - 1];
+            for (int i = 0; i < Items.Length; i++)
+            {
+                if (i == index)
+                {
+                    continue;
+                }
+
+                shrunk[i] = Items[i];
+            }
+
+            Items = shrunk;
+        }
     }
 }
---
+++ Tests/Chapter02/Examples/Generics/CustomlistTesting/CustomListTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using Chapter02.Examples.CsharpKeywords.Generics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.Chapter02.Examples.Generics.CustomlistTesting
+{
+    public abstract class CustomListTests<T>
+    {
+        protected abstract T First { get; }
+        protected abstract T Second { get; }
+
+        [TestMethod]
+        public void New_CreatesEmptyList()
+        {
+            var list = new CustomList<T>();
+
+            Assert.AreEqual(0, list.Count);
+        }
+
+        [TestMethod]
+        public void New_WhenArray_CreateListWithArrayElements()
+        {
+            T[] array = {First, Second};
+            var list = new CustomList<T>(array);
+
+            CollectionAssert.AreEquivalent(array, list.Items);
+        }
+
+        [TestMethod]
+        public void Add_AddsElementInTheEnd()
+        {
+            var list = new CustomList<T>();
+
+            list.Add(First);
+
+            Assert.AreEqual(list.Items.Last(), First);
+        }
+
+        [TestMethod]
+        public void RemoveAtIndex_When_IndexInBounds_RemovesElementsAtIndex()
+        {
+            T[] array = { First };
+            var list = new CustomList<T>(array);
+
+            list.RemoveAtIndex(0);
+
+            Assert.AreEqual(0, list.Count);
+        }
+
+        [TestMethod]
+        public void RemoveAtIndex_When_IndexOutOfBounds_Throws_IndexOutOfRangeException()
+        {
+            var list = new CustomList<T>();
+
+            void Remove() => list.RemoveAtIndex(0);
+
+            Assert.ThrowsException<IndexOutOfRangeException>(Remove);
+        }
+    }
+}
