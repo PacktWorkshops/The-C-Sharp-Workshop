051cb056e47033135d7e66d71924ab142bdb6821
Almantas Karpavičius <almantusk@gmail.com>
2020-11-20 06:14:46 +0200

    ValueTuple demo

--- Chapter02/Examples/CsharpKeywords/Other/ValueTuple/Demo.cs
+++ Chapter02/Examples/CsharpKeywords/Other/ValueTuple/Demo.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Chapter02.Examples.Abstraction.Players;
+using Chapter02.Examples.Encapsulation;
 
 namespace Chapter02.Examples.CsharpKeywords.Other.ValueTuple
 {
@@ -10,7 +12,23 @@ namespace Chapter02.Examples.CsharpKeywords.Other.ValueTuple
     {
         public static void Run()
         {
+            var dog = new Dog("?");
+            var human = new Human("Thomas");
+            bool isDogKnown = false;
 
+            var values1 = new ValueTuple<Dog, Human, bool>(dog, human, isDogKnown);
+            var values2 = (dog, human, isDogKnown);
+
+            var (d, h, b) = GetDogHumanAndBool();
+        }
+
+        public static (Dog, Human, bool) GetDogHumanAndBool()
+        {
+            var dog = new Dog("?");
+            var human = new Human("Thomas");
+            bool isDogKnown = false;
+
+            return (dog, human, isDogKnown);
         }
     }
 }
