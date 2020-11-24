e2cf339fb383c1e24bb27a1e6e24b88754cf85ac
Mateus De Carvalho Viegas <mateusdecarvalhoviegas@Mateuss-MacBook-Pro.local>
2020-11-24 23:34:50 +0000

    Updated exercise 103

--- Chapter01/Exercises/Exercise103/Program.cs
+++ Chapter01/Exercises/Exercise103/Program.cs
@@ -6,8 +6,11 @@ namespace Exercise103
     {
         static void Main(string[] args)
         {
-            var a = 10;
-            var b = 20;
+            Console.WriteLine("Type a value for a: ");
+            var a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Now type a value for b: ");
+            var b = int.Parse(Console.ReadLine());
+            
             Console.WriteLine($"The value for a is { a } and for b is { b }");
             Console.WriteLine($"Sum: { a + b}");
             Console.WriteLine($"Multiplication: { a * b}");
