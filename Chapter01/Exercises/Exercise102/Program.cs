e097d6252e5120dc0944df4c33cfdd52f05acbb3
Mateus De Carvalho Viegas <mateusdecarvalhoviegas@Mateuss-MacBook-Pro.local>
2020-11-24 23:37:47 +0000

    Updated exercise 102 to use top level statements

--- Chapter01/Exercises/Exercise102/Program.cs
+++ Chapter01/Exercises/Exercise102/Program.cs
@@ -1,14 +1,7 @@
 ﻿using System;
 
-namespace Exercise102
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hi! I’m your first Program. What is your name?!");
-            var name = Console.ReadLine();
-            Console.WriteLine($"Hi {name}, it is very nice to meet you.We have a really fun journey ahead.");
-        }
-    }
-}
+Console.WriteLine("Hi! I’m your first Program. What is your name?!");
+
+var name = Console.ReadLine();
+
+Console.WriteLine($"Hi {name}, it is very nice to meet you.We have a really fun journey ahead.");
