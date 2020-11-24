b0a41af80d01be4bd7a19277dacca596a44bc83c
Mateus De Carvalho Viegas <mateusdecarvalhoviegas@Mateuss-MacBook-Pro.local>
2020-11-24 23:37:23 +0000

    Updated exercise 103 to use top level statements

--- Chapter01/Exercises/Exercise103/Program.cs
+++ Chapter01/Exercises/Exercise103/Program.cs
@@ -1,22 +1,12 @@
 ﻿using System;
 
-namespace Exercise103
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Type a value for a: ");
-            var a = int.Parse(Console.ReadLine());
-            Console.WriteLine("Now type a value for b: ");
-            var b = int.Parse(Console.ReadLine());
-            
-            Console.WriteLine($"The value for a is { a } and for b is { b }");
-            Console.WriteLine($"Sum: { a + b}");
-            Console.WriteLine($"Multiplication: { a * b}");
-            Console.WriteLine($"Subtraction: { a - b}");
-            Console.WriteLine($"Division: { a / b}");
+Console.WriteLine("Type a value for a: ");
+var a = int.Parse(Console.ReadLine());
+Console.WriteLine("Now type a value for b: ");
+var b = int.Parse(Console.ReadLine());
 
-        }
-    }
-}
+Console.WriteLine($"The value for a is { a } and for b is { b }");
+Console.WriteLine($"Sum: { a + b}");
+Console.WriteLine($"Multiplication: { a * b}");
+Console.WriteLine($"Subtraction: { a - b}");
+Console.WriteLine($"Division: { a / b}");
