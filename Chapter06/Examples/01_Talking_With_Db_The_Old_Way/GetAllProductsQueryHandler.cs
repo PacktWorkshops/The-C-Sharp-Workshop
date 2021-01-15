75b4bddc850d0e6dbf0ba886d9068892d8494c9a
Almantas Karpavičius <almantusk@gmail.com>
2021-01-15 06:36:57 +0200

    Added Chapter06

---
+++ Chapter06/Examples/01_Talking_With_Db_The_Old_Way/GetAllProductsQueryHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Chapter06.Examples._01_Talking_With_Db_The_Old_Way
+{
+    public class GetAllProductsQueryHandler
+    {
+        public IEnumerable<Product> GetAllProducts()
+        {
+            var connection = new SqlConnection("");
+            return null;
+        }
+    }
+}
