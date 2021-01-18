5329882ca5b1cb2faa24a9ee6729af73150ad346
Almantas Karpavičius <almantusk@gmail.com>
2021-01-18 04:26:00 +0200

    Hid connection string

--- Chapter06/Program.cs
+++ Chapter06/Program.cs
@@ -1,20 +1,15 @@
 ﻿using System;
-using Chapter06.Examples._01_Talking_With_Db_The_Old_Way;
+using Chapter06.Examples.TalkingWithDb.Orm;
 
 namespace Chapter06
 {
     class Program
     {
-        public const string ConnectionString = "Server=ak2020.database.windows.net;Database=GlobalFactory2020;User Id=AlmantasK;Password=Mocking-Bird415;";
+        public static string ConnectionString { get; } = Environment.GetEnvironmentVariable("GlobalFactory", EnvironmentVariableTarget.User);
 
         static void Main(string[] args)
         {
-            var query = new GetAllProductsQueryHandler();
-            var products = query.GetAllProducts();
-            foreach (var product in products)
-            {
-                Console.WriteLine($"{product.Name} {product.Id} {product.Price}");
-            }
+            Demo.Run();
         }
     }
 }
