10cb2aa552ed54a540546334c651727444ea2914
Almantas Karpavičius <almantusk@gmail.com>
2021-01-21 08:17:41 +0200

    Added exercise01 database tables creation script

---
+++ Chapter06/Exercises/Exercise01/GlobalFactory.sql
@@ -0,0 +1,41 @@
+﻿/****** Object:  Schema [Factory]    Script Date: 1/21/2021 8:13:51 AM ******/
+CREATE SCHEMA [Factory]
+GO
+/****** Object:  Table [Factory].[Manufacturer]    Script Date: 1/21/2021 8:13:51 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [Factory].[Manufacturer](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NOT NULL,
+	[Country] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_Manufacturer2] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [Factory].[Product]    Script Date: 1/21/2021 8:13:51 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [Factory].[Product](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NOT NULL,
+	[Price] [money] NOT NULL,
+	[ManufacturerId] [int] NOT NULL,
+ CONSTRAINT [PK_Product_1] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [Factory].[Product]  WITH CHECK ADD  CONSTRAINT [FK_Product_Manufacturer] FOREIGN KEY([ManufacturerId])
+REFERENCES [Factory].[Manufacturer] ([Id])
+ON UPDATE CASCADE
+ON DELETE CASCADE
+GO
+ALTER TABLE [Factory].[Product] CHECK CONSTRAINT [FK_Product_Manufacturer]
+GO
