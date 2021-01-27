2870644d04214698c6bf7a2afc79766948ea7cf8
JasonHales <jason_hales@yahoo.com>
2021-01-27 19:43:32 +0000

    chapter 5 tidy up

--- Chapter05/Examples/AsyncExamples.cs
+++ Chapter05/Examples/AsyncExamples.cs
@@ -7,27 +7,27 @@ namespace Chapter05.Examples
     {
         public static async Task Main()
         {
-            global::Chapter05.Examples.Logger.Log("Starting");
+            Logger.Log("Starting");
             await BuildGreetings();
 
-            global::Chapter05.Examples.Logger.Log("Press Enter");
+            Logger.Log("Press Enter");
             Console.ReadLine();
         }
 
         private static async Task BuildGreetings()
         {
             var message = "Morning";
-            global::Chapter05.Examples.Logger.Log($"{message}");
+            Logger.Log($"{message}");
 
             await Task.Delay(TimeSpan.FromSeconds(10D));
 
             message += "...Afternoon";
-            global::Chapter05.Examples.Logger.Log($"{message}");
+            Logger.Log($"{message}");
 
             await Task.Delay(TimeSpan.FromSeconds(1D));
 
             message += "...Evening";
-            global::Chapter05.Examples.Logger.Log($"{message}");
+            Logger.Log($"{message}");
         }
     }
 }
