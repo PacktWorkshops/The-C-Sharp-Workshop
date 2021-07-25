6b295fb9ce83555ef9bf266598ac21e362963f4a
AlmantasK <almantusk@gmail.com>
2021-07-25 14:16:33 +0300

    Added SQLite example

--- Chapter06/Examples/Repository/Demo.cs
+++ Chapter06/Examples/Repository/Demo.cs
@@ -3,22 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Chapter06.Examples.TalkingWithDb.Orm;
+using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 
 namespace Chapter06.Examples.Repository
 {
     public static class Demo
     {
-        public static void Test()
+        public static void TestInMemory()
         {
             var builder = new DbContextOptionsBuilder<FactoryDbContext>();
             builder.UseInMemoryDatabase(Guid.NewGuid().ToString());
             var options = builder.Options;
             var db = new FactoryDbContext(options);
 
-            var productsRepository = new Repository<Product>(db);
+            Test(db);
+        }
 
-            var product = new Product {Name = "Test PP", ManufacturerId = 1, Price = 9.99m};
+        public static void TestSqlite()
+        {
+            var connection = new SqliteConnection("Filename=:memory:");
+            connection.Open();
+            
+            var builder = new DbContextOptionsBuilder<FactoryDbContext>();
+            builder.UseSqlite(connection);
+            var options = builder.Options;
+            var db = new FactoryDbContext(options);
+            db.Database.EnsureCreated();
+            
+            Test(db);
+
+            connection.Dispose();
+        }
+
+        private static void Test(FactoryDbContext db)
+        {
+            var productsRepository = new Repository<Product>(db);
+            var manufacturer = new Manufacturer() { Id = 1 };
+            db.Manufacturers.Add(manufacturer);
+            db.SaveChanges();
+
+            var product = new Product { Name = "Test PP", ManufacturerId = 1, Price = 9.99m };
             var id = productsRepository.Create(product);
 
             product.Price = 19m;
