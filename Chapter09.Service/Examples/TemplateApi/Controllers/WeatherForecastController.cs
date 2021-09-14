22771e6353cae7e82f3df68cbe838a8be6bdc833
AlmantasK <almantusk@gmail.com>
2021-09-14 23:04:50 +0300

    Added an example of ILoggerFactory instead of generic logger in DI

--- Chapter09.Service/Examples/TemplateApi/Controllers/WeatherForecastController.cs
+++ Chapter09.Service/Examples/TemplateApi/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -8,17 +9,35 @@ namespace Chapter09.Service.Examples.TemplateApi.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILoggerFactory logger)
         {
-            _logger = logger;
+            _logger = logger.CreateLogger(typeof(WeatherForecastController).FullName);
         }
 
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
             return new List<WeatherForecast>(){new WeatherForecast()};
+        }
+
+        [HttpGet("error")]
+        public IEnumerable<WeatherForecast> GetError()
+        {
+            _logger.LogError("Whoops");
+            throw new Exception("Something went wrong");
+        }
+
+        [HttpGet("weekday/{day}")]
+        public IActionResult GetWeekday(int day)
+        {
+            if (day < 1 || day > 7)
+            {
+                return NotFound($"'{day}' is not a valid day of a week.");
+            }
+
+            return Ok(new WeatherForecast());
         }
     }
 }
--- Chapter09.Service/Examples/TemplateApi/Startup.cs
+++ Chapter09.Service/Examples/TemplateApi/Startup.cs
@@ -22,10 +22,14 @@ namespace Chapter09.Service.Examples.TemplateApi
             services.AddControllers();
             services.AddLogging(builder =>
             {
+                builder.ClearProviders();
                 builder.AddConsole();
                 builder.AddDebug();
             });
+            //services.AddSingleton<ILoggerFactory, LoggerFactory>();
         }
+
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
