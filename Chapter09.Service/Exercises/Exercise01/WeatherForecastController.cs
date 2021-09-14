7eb6c13eec2daa38d7c978a6375ce33224d1011c
AlmantasK <almantusk@gmail.com>
2021-09-14 21:07:49 +0300

    Exercise01

---
+++ Chapter09.Service/Exercises/Exercise01/WeatherForecastController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Chapter09.Service.Examples.TemplateApi;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Chapter09.Service.Exercises.Exercise01
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TimeController : ControllerBase
+    {
+        [HttpGet("current")]
+        public IActionResult GetCurrentTime()
+        {
+            return Ok(DateTime.Now.ToString("o"));
+        }
+    }
+}
