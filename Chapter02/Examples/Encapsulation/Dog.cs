f53c34c72af4836784c67ed3ece4ecac45c3f9ef
AlmantasK <almantusk@gmail.com>
2022-01-02 22:33:55 +0200

    Added default ctor in dog class

--- Chapter02/Examples/Encapsulation/Dog.cs
+++ Chapter02/Examples/Encapsulation/Dog.cs
@@ -1,8 +1,10 @@
+using System;
+
 namespace Chapter02.Examples.Encapsulation
 {
     public class Dog
     {
-        public string Name {get;}
+        public string Name { get; }
 
         private string _owner;
         public string Owner
@@ -23,6 +25,13 @@ namespace Chapter02.Examples.Encapsulation
         //{
         //    return _owner;
         //}
+
+        // Not very practical, used for demo purposes.
+        // Constructor 
+        public Dog()
+        {
+            Console.WriteLine("A Dog object has been created");
+        }
 
         public Dog(string name)
         {
