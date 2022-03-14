4c330968d98106b20f3dd01d4f10c808069eff73
AlmantasK <almantusk@gmail.com>
2022-03-14 06:09:06 +0200

    Removed CustomList example

--- Chapter02/Examples/CsharpKeywords/Generics/Demo.cs
+++ Chapter02/Examples/CsharpKeywords/Generics/Demo.cs
@@ -12,20 +12,13 @@ namespace Chapter02.Examples.CsharpKeywords.Generics
             CollectionsPrinterv1.PrintG(new object());
             CollectionsPrinterv1.PrintG("Hey");
 
-            CollectionsPrinterv1.Print(new []{1,23,4,-1});
+            CollectionsPrinterv1.Print(new[] { 1, 23, 4, -1 });
 
             int max1 = (int)Comparator.Max1(3, -4);
             int max2 = Comparator.Max2(3, -4);
 
             Console.WriteLine($"max1 = {max1} " +
                               $"max2 = {max2} ");
-
-            var list = new CustomList<int>();
-            list.Add(1);
-            list.Add(2);
-            list.RemoveAtIndex(1);
-            CollectionsPrinterv1.Print(list.GetAtIndex(0));
-
 
             var numbers = new List<int>();
             numbers.Add(1);
