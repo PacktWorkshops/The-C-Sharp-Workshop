8854b53cad78f63dc4780e75d4b8a73c0d8cfd5d
Mateus Viegas <mateuscviegas@gmail.com>
2022-05-08 13:09:54 +0100

    Exercise03

---
+++ Chapter07/Exercise1_03/ToDoListApp/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ToDoListApp.Models;
+
+namespace ToDoListApp.Pages;
+
+public class IndexModel : PageModel
+{
+    public IList<ToDoTask> Tasks { get; set; } = new List<ToDoTask>();
+
+    public IndexModel()
+    {
+    }
+
+    public void OnGet()
+    {
+        Tasks = new List<ToDoTask>
+            {
+                new ToDoTask("Create", ETaskStatus.ToDo),
+                new ToDoTask("Creating", ETaskStatus.Doing),
+                new ToDoTask("Created", ETaskStatus.Done),
+            };
+    }
+}
