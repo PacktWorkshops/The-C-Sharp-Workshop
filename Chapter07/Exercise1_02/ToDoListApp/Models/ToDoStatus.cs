3976318f4b158fab60ae566ecc7306fbf34de6d4
Mateus Viegas <mateuscviegas@gmail.com>
2022-05-08 13:04:23 +0100

    Added Exercise02

---
+++ Chapter07/Exercise1_02/ToDoListApp/Models/ToDoStatus.cs
@@ -0,0 +1,23 @@
+namespace ToDoListApp.Models;
+
+public class ToDoTask
+{
+    private ToDoTask()
+    {
+        CreatedAt = DateTime.UtcNow;
+        Id = Guid.NewGuid();
+    }
+
+    public ToDoTask(string title, ETaskStatus status) : this()
+    {
+        Title = title;
+        Status = status;
+    }
+
+    public Guid Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? DueTo { get; set; }
+    public string Title { get; set; }
+    public string? Description { get; set; }
+    public ETaskStatus Status { get; set; }
+}
