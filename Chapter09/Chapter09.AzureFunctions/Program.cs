4fc2ca30ea6bbe588652f24a1a846a54c8a20ff8
AlmantasK <almantusk@gmail.com>
2022-05-15 17:56:49 +0300

    Migrate chapter09.AzureFunctions to .NET 6

--- Chapter09/Chapter09.AzureFunctions/Program.cs
+++ Chapter09/Chapter09.AzureFunctions/Program.cs
@@ -1,19 +1,7 @@
-using System.Threading.Tasks;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Azure.Functions.Worker.Configuration;
 
-namespace Chapter09.AzureFunctions
-{
-    public class Program
-    {
-        public static void Main()
-        {
-            var host = new HostBuilder()
-                .ConfigureFunctionsWorkerDefaults()
-                .Build();
+var host = new HostBuilder()
+    .ConfigureFunctionsWorkerDefaults()
+    .Build();
 
-            host.Run();
-        }
-    }
-}
+host.Run();
