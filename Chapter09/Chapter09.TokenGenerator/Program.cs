9b196453adb430221f509c75ebade110045bd6b7
AlmantasK <almantusk@gmail.com>
2022-05-15 18:18:50 +0300

    Removed unnecessary client id

--- Chapter09/Chapter09.TokenGenerator/Program.cs
+++ Chapter09/Chapter09.TokenGenerator/Program.cs
@@ -11,7 +11,6 @@ static IPublicClientApplication BuildAadClientApplication()
 {
     // Uncomment either (double check whether they match your AAD values).
     //const string clientId = "0aabad2a-a997-4cc7-b750-56d905e28d8d"; // Activity01
-    // const string clientId = "2d8834d3-6a27-47c9-84f1-0c9db3eeb4bb";
     const string clientId = "2d8834d3-6a27-47c9-84f1-0c9db3eeb4bb"; // Service
     const string tenantId = "ddd0fd18-f056-4b33-88cc-088c47b81f3e";
     const string redirectUri = "http://localhost:5002/token";
