95dd36fdbab47f5890f8253f87d4e30e7599b655
AlmantasK <almantusk@gmail.com>
2022-05-16 23:01:57 +0300

    Removed warning

--- Chapter06/Examples/AdventureWorksProduction/AdventureworksContext.cs
+++ Chapter06/Examples/AdventureWorksProduction/AdventureworksContext.cs
@@ -46,7 +46,6 @@ namespace Chapter06.Examples.AdventureWorksProduction
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseNpgsql(Program.AdventureWorksConnectionString);
             }
         }
