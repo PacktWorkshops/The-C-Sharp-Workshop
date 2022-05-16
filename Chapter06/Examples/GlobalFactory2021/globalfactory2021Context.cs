daa016dfb0064667da71cd7154825e850f770ab1
AlmantasK <almantusk@gmail.com>
2022-05-16 21:25:26 +0300

    Removed warning

--- Chapter06/Examples/GlobalFactory2021/globalfactory2021Context.cs
+++ Chapter06/Examples/GlobalFactory2021/globalfactory2021Context.cs
@@ -24,7 +24,6 @@ namespace Chapter06.Examples.GlobalFactory2021
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseNpgsql(Program.GlobalFactoryConnectionString);
             }
         }
