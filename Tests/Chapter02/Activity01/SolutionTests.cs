7ba83abd389e96248151c04c210b2336da4a9a07
AlmantasK <almantusk@gmail.com>
2022-09-14 22:42:54 +0300

    Fixed Add_TwoCircles_Returns_CircleWithAreaOfBoth

--- Tests/Chapter02/Activity01/SolutionTests.cs
+++ Tests/Chapter02/Activity01/SolutionTests.cs
@@ -8,22 +8,17 @@ namespace Tests.Chapter02.Activity01
     public class SolutionTests
     {
         [DataTestMethod]
-        [DynamicData(nameof(AddingTwoCirclesExpectations))]
-        public void Add_TwoCircles_Returns_CircleWithAreaOfBoth(Circle circle1, Circle circle2, double expectedNewRadius)
+        [DataRow(0, 0, 0)]
+        [DataRow(0.5, 2, 2.06155)]
+        [DataRow(3, 3, 4.24264)]
+        public void Add_TwoCircles_Returns_CircleWithAreaOfBoth(double radius1, double radius2, double expectedNewRadius)
         {
+            var circle1 = new Circle(radius1);
+            var circle2 = new Circle(radius2);
+
             var newRadius = (circle1 + circle2).Radius;
 
             Assert.AreEqual(expectedNewRadius, newRadius, 0.001);
-        }
-
-        public static IEnumerable<object[]> AddingTwoCirclesExpectations
-        {
-            get
-            {
-                yield return new object[] { new Circle(0), new Circle(0), 0 };
-                yield return new object[] { new Circle(0.5), new Circle(2), 2.06155 };
-                yield return new object[] { new Circle(3), new Circle(3), 4.24264 };
-            }
         }
     }
 }
